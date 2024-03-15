fb5c6b629dd519b28697f555e2aa714ae7fc3a54
meyricks <abrahamer122@gmail.com>
2024-03-14 23:52:53 -0400

    Added: some login styles

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,23 @@
 import Image from '../assets/images/marvelpic.jpg';
 import styles from '../styles/pages/login-page.module.css'
 import Button from '../components/ui/button'
+import Marvel from '../assets/images/marvel.svg'
 
 export default function login() {
   return (
     <div className={styles.container}>
-      <img src={Image} alt="Comics Image"/>
-      <p>Join the amazing universe of its characters.</p>
-      <Button text="Login" link="#"/>
+      <article className={styles.left}>
+        <img src={Image} alt="Comics Image"/>
+
+      </article>
+
+      <article className={styles.right}>
+        <img className={styles.marvel} src={Marvel} alt="Comics Image"/>
+        <p className={styles.text}>Join the amazing universe of its characters.</p>
+        <Button text="Login" link="#"/>
+      </article>
+  
+    
     </div>
   )
 }
